beba53a3c70ae99626a205a7779dad2e7e3430ff
Sela van Vreeswijk <102545946+selavanvreeswijk@users.noreply.github.com>
2024-05-28 09:35:20 +0200

    additionele files

---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
+
+function scrollToContent() {
+    document.getElementById('content').scrollIntoView({ behavior: 'smooth' });
+}
+
+console.log('test')
